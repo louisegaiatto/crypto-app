fed6cc0b849dbffdb8f7ba37222f74e6ad0d4725
Louise Gaiatto <louisegaiatto@Louises-MacBook-Pro.local>
2019-11-10 17:26:37 -0300

    First commit

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Modules to control application life and create native browser window
-const {app, BrowserWindow} = require('electron')
+const {app, BrowserWindow, Menu} = require('electron')
 const path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
@@ -34,6 +34,18 @@ function createWindow () {
     // when you should delete the corresponding element.
     mainWindow = null
   })
+
+  var menu = Menu.buildFromTemplate([
+    {
+        label: 'Menu',
+        submenu: [
+            {label:'Adjust Notification Value'},
+            {label:'CoinMarketCap'},
+            {label:'Exit'}
+        ]
+    }
+])
+Menu.setApplicationMenu(menu); 
 }
 
 // This method will be called when Electron has finished
--- next/pages/index.js
+++ next/pages/index.js
@@ -1,5 +1,47 @@
-function Home() {
-    return <div>Welcome to Next.js!</div>
-}
+import fetch from 'isomorphic-fetch'
 
-export default Home
+export default class Home extends React.Component {
+
+    state = {
+        username: null,
+        user: null
+    }
+
+    handleSumit = e => {
+        e.preventDefault()
+
+        const {username} = this.state
+
+        fetch('https://api.github.com/users/${username}')
+        .then(res => res.json())
+        .then(user => this.setState({user}))
+        .catch(err => console.error(err))
+    }
+
+
+      
+    render() {
+        const {user} = this.state
+
+
+        return (
+            <div>
+                {user && (
+                    <div>
+                        <img src={user.avatar_url} alt={user.login} width={50} />
+                        <h3>{user.name}</h3>
+                        <p>{user.company}</p>
+                    </div>
+                )}
+                <form onSubmit={this.handleSubmit}>
+                <input 
+                    type="text" 
+                    placeholder="Type GitHub username..." 
+                    onChange={e => this.setState({username: e.target.value})}
+                    />
+                    <button type="submit">Go!</button>
+                </form>
+            </div>
+        )
+    }
+}
